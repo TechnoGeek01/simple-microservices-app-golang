78ae03a2f439bfee94a4f403ac1b3f5bf1c246eb
TechnoGeek01 <rsudharsangames@gmail.com>
2023-11-03 22:52:42 +0530

    added connection function to rabbitmq and tested

--- listener-service/main.go
+++ listener-service/main.go
@@ -1,11 +1,59 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
 func main() {
 	// try to connect to rabbitmq
+	rabbitConn, err := connect()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	defer rabbitConn.Close()
+
+	log.Println("COnnected to RabbitMQ")
 
 	// start listening for messages
 
 	// create consumer
 
 	// watch the queue and consume events
+}
+
+func connect() (*amqp.Connection, error) {
+	var counts int64
+	var backoff = 1 * time.Second
+	var connection *amqp.Connection
+
+	// don't continue until rabbitmq is ready
+	for {
+		c, err := amqp.Dial("amqp://guest:guest@localhost")
+		if err != nil {
+			fmt.Println("RabbitMQ not yet ready...")
+			counts++
+		} else {
+			connection = c
+			break
+		}
+
+		if counts > 5 && err == nil {
+			log.Printf("retried more than %v times", counts)
+			return nil, fmt.Errorf("retried more than %v times", counts)
+		}
+
+		backoff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+		log.Println("backing off...")
+		time.Sleep(backoff)
+		continue
+	}
+
+	return connection, nil
 }
